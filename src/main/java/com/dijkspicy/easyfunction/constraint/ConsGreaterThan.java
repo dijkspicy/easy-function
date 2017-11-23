9c5dbf2ad618e873d04cd009b0a4301b34c1eb73
dijkpicy <dijkspicy@outlook.com>
2017-11-23 18:58:03 +0800

    Update ConsGreaterThan.java

--- src/main/java/com/dijkspicy/easyfunction/constraint/ConsGreaterThan.java
+++ src/main/java/com/dijkspicy/easyfunction/constraint/ConsGreaterThan.java
@@ -7,4 +7,9 @@ package com.dijkspicy.easyfunction.constraint;
  * @Date 2017/11/21
  */
 public class ConsGreaterThan {
+    @Override
+    public boolean check(Object presentValue){
+        return (this.expectedValue instanceof Comparable && presentValue instanceof Comparable)
+                && ((Comparable) presentValue).compareTo(this.expectedValue) > 0;
+    }
 }
